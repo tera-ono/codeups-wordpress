5ad5a21032f3992f00527bbc7228c579393d6859
tera-ono <95453431+tera-ono@users.noreply.github.com>
2022-02-16 18:29:01 +0900

    footer.php 完了

---
+++ WordPressTheme/includes/contact_section.php
@@ -0,0 +1,21 @@
+<!-- お問い合わせページ以外に表示されるセクション footer.phpに含める -->
+<section class="l-commonContact c-commonContact">
+  <div class="l-inner">
+    <!-- セクションタイトル -->
+    <div class="c-commonContact__title">
+      <h3 class="c-sectionTitle">
+        お問い合わせ
+        <span class="c-sectionTitle__sub">Contact</span>
+      </h3>
+    </div>
+    <p class="c-commonContact__text">
+      テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+    </p>
+    <div class="c-commonContact__link">
+      <a href="<?php echo esc_url(home_url('contact')); ?>" class="c-btn">
+        詳しくみる
+      </a>
+    </div>
+  </div>
+</section>
+<!-- /.c-commonContact -->
--- WordPressTheme/includes/footer/content.php
+++ WordPressTheme/includes/footer/content.php
@@ -1,18 +1,35 @@
+<!-- お問い合わせページ以外に表示されるセクション footer.phpに含める -->
+<?php get_template_part('includes/contact_section'); ?>
+
+
 <!-- フッターの中身 -->
 <footer class="l-footer p-footer">
+  <div class="p-footer__inner">
+    <div class="p-footer__items">
+      <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__logo">
+        <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/common/logo.svg'); ?>" alt="">
+      </a>
 
-  <div class="p-footer__inner l-inner">
-    <nav class="p-footer__nav c-footer-nav">
-      <?php
-      wp_nav_menu(
-        array(
-          'depth' => 1,
-          'theme_location' => 'global', //グローバルメニューをここに表示すると指定
-          'container' => false,
-          'menu_class' => 'c-footer-nav__items', //ulのクラス
-        )
-      );
-      ?>
-    </nav>
+      <nav class="p-footer__nav c-footerNav">
+        <?php
+        wp_nav_menu(
+          array(
+            'depth' => 1,
+            'theme_location' => 'footer', //フッターメニューをここに表示すると指定
+            'container' => false,
+            'menu_class' => 'c-footerNav__items c-navItems', //ulのクラス
+            'link_before' => '<span>',
+            'link_after' => '</span>',
+          )
+        );
+        ?>
+      </nav>
+    </div>
+    <p class="p-footer__copyright"> © 2021 CodeUps Inc.</p>
   </div>
 </footer>
+
+<!-- スクロールトップボタン -->
+<div class="c-scrollTop js-scrollTop">
+  <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/common/scrollTop.png'); ?>" alt="">
+</div>
