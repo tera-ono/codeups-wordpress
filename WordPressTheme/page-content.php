5ed6c698576f8556e72169c50b78ac825060054d
tera-ono <95453431+tera-ono@users.noreply.github.com>
2022-02-25 21:38:23 +0900

    事業内容 固定ページ完了

--- WordPressTheme/page-content.php
+++ WordPressTheme/page-content.php
@@ -1,3 +1,59 @@
 <?php get_header(); ?>
-事業内容のページ
+<section class="l-content p-content">
+  <div class="p-content__inner l-inner">
+
+    <div class="p-content__heading">
+      <h3 class="p-content__title">企業理念</h3>
+      <p class="p-content__text">
+        私たちは、変化を恐れずに常にその時のベストな方法を模索します。<br>
+        クライアント様の目的を達成するための最新の技術やツールを取り入れ、また私たち自身が働く場所にとらわれずに心と体が穏やかに過ごせるところで仕事をすることを推奨し、それが成果に還元出来ると信じております。
+      </p>
+    </div>
+
+    <div class="p-content__items">
+
+      <div class="p-content__item p-contentItem">
+        <div class="p-contentItem__img">
+          <img src="<?php echo esc_url(get_template_directory_uri() . "/assets/images/common/top-contents1.jpg"); ?>" alt="PC作業">
+        </div>
+        <div class="p-contentItem__body">
+          <h4 class="p-contentItem__title">企業理念１</h4>
+          <p class="p-contentItem__text">
+            私たちは、変化を恐れずに常にその時のベストな方法を模索します。<br>
+            クライアント様の目的を達成するための最新の技術やツールを取り入れ、また私たち自身が働く場所にとらわれずに心と体が穏やかに過ごせるところで仕事をすることを推奨し、それが成果に還元出来ると信じております。
+          </p>
+        </div>
+      </div>
+      
+      <div class="p-content__item p-contentItem">
+        <div class="p-contentItem__img">
+          <img src="<?php echo esc_url(get_template_directory_uri() . "/assets/images/common/top-contents2.jpg"); ?>" alt="PC作業">
+        </div>
+        <div class="p-contentItem__body">
+          <h4 class="p-contentItem__title">企業理念2</h4>
+          <p class="p-contentItem__text">
+            私たちは、変化を恐れずに常にその時のベストな方法を模索します。<br>
+            クライアント様の目的を達成するための最新の技術やツールを取り入れ、また私たち自身が働く場所にとらわれずに心と体が穏やかに過ごせるところで仕事をすることを推奨し、それが成果に還元出来ると信じております。
+          </p>
+        </div>
+      </div>
+
+      <div class="p-content__item p-contentItem">
+        <div class="p-contentItem__img">
+          <img src="<?php echo esc_url(get_template_directory_uri() . "/assets/images/common/top-contents3.jpg"); ?>" alt="PC作業">
+        </div>
+        <div class="p-contentItem__body">
+          <h4 class="p-contentItem__title">企業理念3</h4>
+          <p class="p-contentItem__text">
+            私たちは、変化を恐れずに常にその時のベストな方法を模索します。<br>
+            クライアント様の目的を達成するための最新の技術やツールを取り入れ、また私たち自身が働く場所にとらわれずに心と体が穏やかに過ごせるところで仕事をすることを推奨し、それが成果に還元出来ると信じております。
+          </p>
+        </div>
+      </div>
+
+    </div><!-- p-content__items -->
+
+  </div>
+</section>
+<!-- /.l-content p-content -->
 <?php get_footer(); ?>
