92d93304f4f18992d6dc242d916af196bb925c2d
tera-ono <95453431+tera-ono@users.noreply.github.com>
2022-03-02 07:44:54 +0900

    カスタム投稿ブログ タクソノミーアーカイブ完了

--- WordPressTheme/archive-blog.php
+++ WordPressTheme/archive-blog.php
@@ -1,21 +1,30 @@
 <?php get_header(); ?>
-ブログ一覧ページ
-<div class="l-inner">
-  <!-- パーツ化したリンクタブ -->
-  <?php get_template_part('includes/blog_linkTab'); ?>
+<main>
+  <section class="l-blog p-blog">
+    <div class="p-blog__inner l-inner">
+      <!-- パーツ化したリンクタブ -->
+      <?php get_template_part('includes/blog_linkTab'); ?>
 
-  <div class="c-cards">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <!-- パーツ化したブログカード -->
-        <?php get_template_part('includes/blog_card'); ?>
+      <div class="p-blog__cards c-cards">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php if ($wp_query->current_post == 0) : ?>
+              <?php get_template_part('includes/newIcon_blog_card'); ?>
+            <?php else : ?>
+              <!-- パーツ化したブログカード -->
+              <?php get_template_part('includes/blog_card'); ?>
+            <?php endif; ?>
 
-    <?php endwhile;
-    endif; ?>
+        <?php endwhile;
+        endif; ?>
 
-    <!-- パーツ化したページネーション -->
-    <?php get_template_part('includes/pagination'); ?>
-  </div>
-</div>
+      </div>
+      <!-- パーツ化したページネーション -->
+      <?php get_template_part('includes/pagination'); ?>
+    </div>
+  </section>
+  <!-- /.l-blog p-blog -->
+</main>
+
 
 
 <?php get_footer(); ?>
--- WordPressTheme/taxonomy-blog_category.php
+++ WordPressTheme/taxonomy-blog_category.php
@@ -1,14 +1,30 @@
 <?php get_header(); ?>
-ブログターム毎の一覧ページ
-<!-- パーツ化したリンクタブ -->
-<?php get_template_part('includes/blog_linkTab'); ?>
+<main>
+  <section class="l-blog p-blog">
+    <div class="p-blog__inner l-inner">
+      <!-- パーツ化したリンクタブ -->
+      <?php get_template_part('includes/blog_linkTab'); ?>
 
-<?php if(have_posts()): while(have_posts()): the_post(); ?>
-<!-- パーツ化したブログカード -->
-<?php get_template_part('includes/blog_card'); ?>
+      <div class="p-blog__cards c-cards">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php if ($wp_query->current_post == 0) : ?>
+              <?php get_template_part('includes/newIcon_blog_card'); ?>
+            <?php else : ?>
+              <!-- パーツ化したブログカード -->
+              <?php get_template_part('includes/blog_card'); ?>
+            <?php endif; ?>
 
-<?php endwhile; endif; ?>
+        <?php endwhile;
+        endif; ?>
 
-<!-- パーツ化したページネーション -->
-<?php get_template_part('includes/pagination'); ?>
+      </div>
+      <!-- パーツ化したページネーション -->
+      <?php get_template_part('includes/pagination'); ?>
+    </div>
+  </section>
+  <!-- /.l-blog p-blog -->
+</main>
+
+
+
 <?php get_footer(); ?>
