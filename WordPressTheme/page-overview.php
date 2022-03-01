5d5effbcdb044a279d0ee3d69ab5bdc0be070cb9
tera-ono <95453431+tera-ono@users.noreply.github.com>
2022-03-02 07:02:34 +0900

    企業概要ページ page-overview完了

--- WordPressTheme/page-overview.php
+++ WordPressTheme/page-overview.php
@@ -1,3 +1,47 @@
 <?php get_header(); ?>
-企業概要のページ
+<main>
+  <section class="l-overview p-overview">
+    <div class="p-overview__inner l-inner">
+      <dl class="p-overview__list p-overviewList">
+        <div class="p-overviewList__item">
+          <dt class="p-overviewList__title">会社名</dt>
+          <dd class="p-overviewList__text">株式会社</dd>
+        </div>
+        <div class="p-overviewList__item">
+          <dt class="p-overviewList__title">設立</dt>
+          <dd class="p-overviewList__text">1990年</dd>
+        </div>
+        <div class="p-overviewList__item">
+          <dt class="p-overviewList__title">資本金</dt>
+          <dd class="p-overviewList__text">2000万円</dd>
+        </div>
+        <div class="p-overviewList__item">
+          <dt class="p-overviewList__title">売上高</dt>
+          <dd class="p-overviewList__text">3000万円</dd>
+        </div>
+        <div class="p-overviewList__item">
+          <dt class="p-overviewList__title">代表者</dt>
+          <dd class="p-overviewList__text">佐藤太郎</dd>
+        </div>
+        <div class="p-overviewList__item">
+          <dt class="p-overviewList__title">従業員数</dt>
+          <dd class="p-overviewList__text">4人</dd>
+        </div>
+        <div class="p-overviewList__item">
+          <dt class="p-overviewList__title">事業内容</dt>
+          <dd class="p-overviewList__text">ホームページ制作・デザイン制作・名刺作成・イベント企画・SNS運用・その他</dd>
+        </div>
+        <div class="p-overviewList__item">
+          <dt class="p-overviewList__title">所在地</dt>
+          <dd class="p-overviewList__text">宮城県仙台市</dd>
+        </div>
+      </dl>
+
+      <div class="p-overview__map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3132.834866007511!2d140.88025417950695!3d38.260135791958824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f8a28180c510b87%3A0xb2a30b91be1ffdbc!2z5LuZ5Y-w6aeF!5e0!3m2!1sja!2sjp!4v1646167229522!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      </div>
+    </div>
+  </section>
+  <!-- /.l-overview p-overview -->
+</main>
 <?php get_footer(); ?>
