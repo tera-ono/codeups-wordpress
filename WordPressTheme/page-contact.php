64fd3337573e202adffb37b10c376ea546d91b89
tera-ono <95453431+tera-ono@users.noreply.github.com>
2022-04-13 13:31:21 +0900

    MW WP Form　HTML　バリデーション設定完了

--- WordPressTheme/page-contact.php
+++ WordPressTheme/page-contact.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
-お問い合わせのページ
+<main>
+  <section class="p-contact l-contact">
+    <div class="p-contact__inner l-inner">
+      <div class="p-contact__form p-contactForm">
+        <?php if (have_posts()) : the_post() ?>
+          <?php the_content(); ?>
+        <?php endif; ?>
+      </div>
+    </div>
+  </section>
+  <!-- /.p-contact l-contact -->
+</main>
 <?php get_footer(); ?>
