94cdc9f72fcf39be192212910d7a66fbf45ab75f
tera-ono <95453431+tera-ono@users.noreply.github.com>
2022-04-19 11:27:09 +0900

    404ページ完了

--- WordPressTheme/404.php
+++ WordPressTheme/404.php
@@ -1,3 +1,21 @@
 <?php get_header(); ?>
-404
+<main>
+  <section class="p-404 l-404">
+    <div class="p-404__inner l-inner">
+      <h2 class="p-404__heading">
+        404 Not Found
+      </h2>
+      <p class="p-404__text">
+        お探しのページは<br class="u-hidden-tab">
+        見つかりませんでした。
+      </p>
+      <div class="p-404__link">
+        <a href=<?php echo esc_url(home_url('/')); ?> class="c-btn">
+          TOPへ
+        </a>
+      </div>
+    </div>
+  </section>
+  <!-- /.p-404 l-404 -->
+</main>
 <?php get_footer(); ?>
--- WordPressTheme/includes/footer/contact_section.php
+++ WordPressTheme/includes/footer/contact_section.php
@@ -1,23 +1,23 @@
-<!-- お問い合わせページ以外に表示されるセクション footer.phpに含める -->
-<?php if(!(is_page('contact') || is_page('thanks'))) : ?>
-<section class="l-commonContact c-commonContact">
-  <div class="l-inner">
-    <!-- セクションタイトル -->
-    <div class="c-commonContact__title">
-      <h3 class="c-sectionTitle">
-        お問い合わせ
-        <span class="c-sectionTitle__sub">Contact</span>
-      </h3>
+<!-- お問い合わせページ(サンクスページも含む)と404ページ以外に表示されるセクション footer.phpに含める -->
+<?php if (!(is_page('contact') || is_page('thanks') || is_404())) : ?>
+  <section class="l-commonContact c-commonContact">
+    <div class="l-inner">
+      <!-- セクションタイトル -->
+      <div class="c-commonContact__title">
+        <h3 class="c-sectionTitle">
+          お問い合わせ
+          <span class="c-sectionTitle__sub">Contact</span>
+        </h3>
+      </div>
+      <p class="c-commonContact__text">
+        テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+      </p>
+      <div class="c-commonContact__link">
+        <a href="<?php echo esc_url(home_url('contact')); ?>" class="c-btn">
+          詳しくみる
+        </a>
+      </div>
     </div>
-    <p class="c-commonContact__text">
-      テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-    </p>
-    <div class="c-commonContact__link">
-      <a href="<?php echo esc_url(home_url('contact')); ?>" class="c-btn">
-        詳しくみる
-      </a>
-    </div>
-  </div>
-</section>
-<!-- /.c-commonContact -->
+  </section>
+  <!-- /.c-commonContact -->
 <?php endif; ?>
